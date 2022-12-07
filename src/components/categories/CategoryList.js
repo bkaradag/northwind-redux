9292a49ba76988539366b9ed0b157d4d46d9fb2b
Berkay Karadag <berkay.karadag@outlook.com>
2022-12-07 12:50:00 +0100

    reducers added

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,9 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
 
-export default class CategoryList extends Component {
+class CategoryList extends Component {
   render() {
     return (
-      <div>CategoryList</div>
-    )
+      <div>
+        <h3>CategoryList {this.props.actions.getCategories().length}</h3>
+        <h5>Secili Kategori:{this.props.currentCategory.categoryName}</h5>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    currentCategory: state.changeCategoryReducer,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      ),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
